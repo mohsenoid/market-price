e093919b899eae8cf6ad3c54aba54426e8fc0479
Mohsen Mirhoseini Argi <mohsen@karinaco.com>
2016-03-27 02:03:01 +0430

    added MainActivity Test

--- app/src/androidTest/java/com/mirhoseini/marketprice/MainActivityTest.java
+++ app/src/androidTest/java/com/mirhoseini/marketprice/MainActivityTest.java
@@ -1,13 +1,23 @@
 package com.mirhoseini.marketprice;
 
+import android.content.Context;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.ActivityTestCase;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 import com.mirhoseini.appsettings.AppSettings;
 import com.mirhoseini.marketprice.ui.main.view.MainActivity;
 import com.mirhoseini.marketprice.utils.Constants;
+import com.mirhoseini.marketprice.utils.TimeSpan;
+
+import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 
 
 /**
@@ -17,56 +27,51 @@ import static android.support.test.espresso.Espresso.onView;
 //@LargeTest
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    public MainActivityTest(Class<MainActivity> activityClass) {
-        super(activityClass);
+    private MainActivity mActivity;
+    private Spinner mSpinner;
+    private SpinnerAdapter mPlanetData;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
     }
 
     public void setUp() throws Exception {
         super.setUp();
-        System.setProperty("dexmaker.dexcache", getInstrumentation().getTargetContext().getCacheDir().getPath());
-        clearSharedPref();
-        getActivity();
+
+        setActivityInitialTouchMode(false);
+        mActivity = getActivity();
+        mSpinner =
+                (Spinner) mActivity.findViewById(R.id.timespan_spinner);
+        mPlanetData = mSpinner.getAdapter();
     }
 
-    private void clearSharedPref() {
-        AppSettings.clearValue(getActivity(), Constants.LAST_TIMESPAN);
+    @Test
+    public void testPreConditions() {
+        assertTrue(mSpinner.getOnItemSelectedListener() != null);
+        assertTrue(mPlanetData != null);
+        assertEquals(mPlanetData.getCount(), TimeSpan.getValues().size());
     }
 
-    public void testLayout() throws Exception {
-        // arrange
-
-        // act
-
-        // assert
-        onView(withId(R.id.progress))
-                .check(ViewAssertions.matches(isDisplayed()));
-        onView(withId(R.id.progress_message))
-                .check(ViewAssertions.matches(isDisplayed()));
-        onView(withId(R.id.enter_button))
-                .check(ViewAssertions.matches(isDisplayed()));
+    @Test
+    public void testSpinnerUI() {
+        mActivity.runOnUiThread(
+                new Runnable() {
+                    public void run() {
+                        mSpinner.requestFocus();
+                        mSpinner.setSelection(TimeSpan.DAY_60.getPosition());
+                    }
+                }
+        );
     }
-//
-//    public void testLoginSuccess() throws Exception {
-//        // arrange
-//        doAnswer(new Answer() {
-//            @Override
-//            public Object answer(InvocationOnMock invocation) throws Throwable {
-//                UserApiService.Callback callback = (UserApiService.Callback) invocation.getArguments()[2];
-//                callback.onSuccess();
-//                return null;
-//            }
-//        }).when(mMockUserService).login(anyString(), anyString(), any(UserApiService.Callback.class));
-//
-//        // act
-//        onView(withId(R.id.email))
-//                .perform(ViewActions.typeText("whateverEmail"));
-//        onView(withId(R.id.password))
-//                .perform(ViewActions.typeText("whateverPassword"));
-//        onView(withId(R.id.enter_button))
-//                .perform(ViewActions.click());
-//
-//        // assert
-//        onView(withText("Second Page"))
-//                .check(ViewAssertions.matches(isDisplayed()));
-//    }
+
+    @Test
+    public void testSpinnerValue() {
+        int mPos = mSpinner.getSelectedItemPosition();
+        String mSelection = TimeSpan.DAY_60.getValue();
+
+        String resultText = mSpinner.getSelectedItem().toString();
+
+        assertEquals(resultText, mSelection);
+    }
+
 }
